17d10c511fc3b6cb67e1b91c2b79d69361a70559
AykutCevik <aykut@bizuweb.com>
2015-07-11 18:22:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # owncloud-fail2ban
-Automated install for owncloud fail2ban configurations 
+
+This is an automated installer for fail2ban, which installs fail2ban and sets the specific owncloud configurations.
+
+## Install
+Just execute the following command and the fail2ban filter for owncloud will be activated and set up:
+
+```curl -s "https://raw.githubusercontent.com/AykutCevik/owncloud-fail2ban/master/automated-install/install.sh" | bash```
+
+
+## Version
+The script has been tested with owncloud 8.0.4 and Ubuntu, but may also run under other versions and distributions. If not, please let me know.
---
+++ automated-install/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Automated installer for owncloud fail2ban configuration.
+#
+# Install with this command on your server:
+# curl -s "https://raw.githubusercontent.com/AykutCevik/owncloud-fail2ban/master/automated-install/install.sh" | bash
+# Or run the commands below in given order.
+#
+# Script may also run under other versions of owncloud.
+
+clear
+echo "  _____      ___ __   ___| | ___  _   _  __| |      / _| __ _(_) |___ \| |__   __ _ _ __  "
+echo " / _ \ \ /\ / / '_ \ / __| |/ _ \| | | |/ _' |_____| |_ / _' | | | __) | '_ \ / _' | '_ \ "
+echo "| (_) \ V  V /| | | | (__| | (_) | |_| | (_| |_____|  _| (_| | | |/ __/| |_) | (_| | | | |"
+echo " \___/ \_/\_/ |_| |_|\___|_|\___/ \__,_|\__,_|     |_|  \__,_|_|_|_____|_.__/ \__,_|_| |_|"
+echo "            __Owncloud 8.0.4 fail2ban installer__"
+echo ""
+
+JAILFILE="/etc/fail2ban/jail.d/owncloud.conf"
+FILTERFILE="/etc/fail2ban/filter.d/owncloud.conf"
+OWNCLOUDLOG=""
+if [[ -f $JAILFILE ]];then
+    echo "Already installed, updating owncloud filter."
+    OWNCLOUDLOG=$(awk -F "=" '/logpath/ {print $2}' $JAILFILE)
+else
+	echo "Installing. Please provide your owncloud log path once. Updates will fetch it automatically."
+    echo "Owncloud logpath: "
+    read OWNCLOUDLOG </dev/tty
+
+    echo "Installing fail2ban if not already done..."
+    sudo apt-get update > /dev/null
+    sudo apt-get -y install fail2ban
+fi
+
+echo "Downloading configurations..."
+sudo curl -o "$FILTERFILE" "https://raw.githubusercontent.com/AykutCevik/owncloud-fail2ban/master/automated-install/fail2ban/filter.owncloud.conf"
+sudo curl -o "$JAILFILE" "https://raw.githubusercontent.com/AykutCevik/owncloud-fail2ban/master/automated-install/fail2ban/jail.owncloud.conf"
+
+echo "Setting log path..."
+echo "logpath = $OWNCLOUDLOG" | sudo tee -a "$JAILFILE"
+
+echo "Activating new owncloud filter. Restarting fail2ban..."
+sudo /etc/init.d/fail2ban restart
+
+echo "Work is done!"
